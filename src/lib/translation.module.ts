0830aed8490d5a0d04fc913b2a9b40be2fe824cc
Iwan van Zijderveld <iwanzijderveld@gmail.com>
2019-08-17 00:46:55 +0200

    initial commit

---
+++ src/lib/translation.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { TranslationService } from './services/translation.service';
+
+@NgModule({
+  providers: [
+    TranslationService
+  ]
+})
+export class TranslationModule { }
